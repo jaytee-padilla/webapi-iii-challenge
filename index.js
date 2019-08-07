26429076a6f9ab71f9c855daf8c416457c83341d
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-08-07 15:37:59 -0700

    Dependencies installed. Project initialized

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code away!
+const express = require('express');
+const server = express();
+
+// setup api server
+server.use(express.json());
+server.get('/', (req, res) => res.send('\nAPI running\n'));
+server.listen(7000, () => console.log('\nAPI running on port 7000\n'));
+
+// routing
+const routes = require('./server');
+server.use('/api', routes);
