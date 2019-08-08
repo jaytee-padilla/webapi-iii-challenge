b01f7d92a1c46523f66c8f8d61b7fd183c8cd74e
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-08-08 14:06:44 -0700

    made changes to package.json for heroku deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server');
 
 // port variable enables port to be dynamic
-const port = 7000;
+const port = process.env.PORT || 7000;
 
-server.listen(port, () => console.log(`\nAPI running on http://localhost:${port}\n`));
+server.listen(port, () => console.log(`\nAPI running on ${port}\n`));
